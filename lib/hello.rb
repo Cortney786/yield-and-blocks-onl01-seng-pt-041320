50798dccae807d8a9a3fa6fd42824016d17d2140
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-21 13:25:39 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,9 +2,17 @@ def hello_t(array)
 i = 0
 
   while i < array.length  
+  yield array[i]
   i = i + 1 
  end
 end
 
 # call your method here!
 
+#["Tim", "Tom", "Jim"]
+
+hello_t({"Tim", "Tom", "Jim"}) do |name|
+  if name.stat_with?("T")
+    puts "Hi, #{name}"
+  end 
+end
